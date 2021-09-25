bb78661a211c549837551709b99bda452fce3fdc
Anis <aniskhan001@outlook.com>
2021-09-25 22:34:11 +0200

    moved server functionality to cmd

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,8 +1,24 @@
 package cmd
 
 import (
-	"university/server"
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
 
+	clubDelivery "university/app/club/delivery"
+	deptDelivery "university/app/department/delivery"
+	studentDelivery "university/app/student/delivery"
+	systemDelivery "university/app/system/delivery"
+	teacherDelivery "university/app/teacher/delivery"
+
+	"university/infrastructure/config"
+	"university/infrastructure/db/mysql"
+	"university/infrastructure/middlewares"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -12,20 +28,58 @@ var serveCmd = &cobra.Command{
 	Short: "serves the web server",
 	Long:  "Start serving the web traffics",
 	Run: func(cmd *cobra.Command, args []string) {
-		server.Serve()
+		serve()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(serveCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+func serve() {
+	// load application configuration
+	if err := config.Load(); err != nil {
+		logrus.Error(err)
+		os.Exit(1)
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// connect to database
+	if err := mysql.Connect(); err != nil {
+		logrus.Errorln(err)
+		os.Exit(1)
+	}
+	db := mysql.Get().DB
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// http server setup
+	e := echo.New()
+
+	// fetch infra and routes, todo: add contexts
+	if err := middlewares.Attach(e); err != nil {
+		logrus.Errorln(err)
+		os.Exit(1)
+	}
+
+	// register endpoints
+	systemDelivery.RegisterEndpoints(e, db)
+	deptDelivery.RegisterEndpoints(e, db)
+	teacherDelivery.RegisterEndpoints(e, db)
+	studentDelivery.RegisterEndpoints(e, db)
+	clubDelivery.RegisterEndpoints(e, db)
+
+	// start http server
+	go func() {
+		e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", config.Get().App.Port)))
+	}()
+
+	// graceful shutdown setup
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+	logrus.Info("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_ = e.Shutdown(ctx)
+	logrus.Infof("server shutdowns gracefully")
 }
